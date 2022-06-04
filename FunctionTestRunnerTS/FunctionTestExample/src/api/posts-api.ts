bcabacc66c4e044055af26bed34b6e558110e688
David Bandemo <david@bandemo.se>
2022-06-04 23:43:08 +0200

    De

--- FunctionTestRunnerTS/FunctionTestExample/src/api/posts-api.ts
+++ FunctionTestRunnerTS/FunctionTestExample/src/api/posts-api.ts
@@ -1,5 +1,9 @@
-import * from 'functiontestrunner';
+import { ApiBase } from 'functiontestrunner';
+import { Post } from '../models/post';
 
 export default class PostsApi extends ApiBase {
+    public createPost(post: Post): Post {
+        this.PostWithBody<Post>(post);
 
+    }
 }
