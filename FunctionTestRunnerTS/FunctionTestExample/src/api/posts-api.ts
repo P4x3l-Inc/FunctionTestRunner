d02a245388d84cec788aa9d495902849ba2fc19c
David Bandemo <david@bandemo.se>
2022-06-05 00:14:34 +0200

    Handle export

--- FunctionTestRunnerTS/FunctionTestExample/src/api/posts-api.ts
+++ FunctionTestRunnerTS/FunctionTestExample/src/api/posts-api.ts
@@ -1,9 +1,12 @@
-import { ApiBase } from 'functiontestrunner';
+import ApiBase from 'functiontestrunner/src/wrappers/api/api-base.js';
 import { Post } from '../models/post';
 
 export default class PostsApi extends ApiBase {
-    public createPost(post: Post): Post {
-        this.PostWithBody<Post>(post);
+    ignoreApiStatusCodes: boolean = false;;
 
+    public async createPost(post: Post): Promise<Post> {
+        const response = await this.PostWithBody<Post>('posts', post);
+
+        return response;
     }
 }
